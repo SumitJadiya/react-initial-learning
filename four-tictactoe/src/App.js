95e726c2c4917ce8cf24d5ee280c2f074dcad999
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-05-19 17:01:05 +0530

    tic tac toe initial setup

---
+++ four-tictactoe/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Icon from "./components/icon"
+import { ToastContainer, toast } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap"
+import 'bootstrap/dist/css/bootstrap.css'
+
+const itemArray = new Array(9).fill("empty")
+
+const App = () => {
+  const [isCross, setIsCross] = useState(false)
+  const [winMessage, setSetMessage] = useState("")
+
+  const reloadGame = () => {
+
+  }
+
+  const checkIsWinner = () => {
+
+  }
+
+  const changeItem = itemNumber => {
+
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Icon />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
