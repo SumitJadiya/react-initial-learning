122383dd98ca2e11b174cbedfe87501e3caf69ab
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-07-05 15:40:28 +0530

    deploy in aws S3

---
+++ ten-firebase-github-app/build/precache-manifest.f47792dda3dce02d13a82363d54f5942.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb351e41365c3782fa955099dc89626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4fa8d8b2e4b84c1e741",
+    "url": "/static/css/2.79dd3d24.chunk.css"
+  },
+  {
+    "revision": "ceac13d08ab9f818f027",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4fa8d8b2e4b84c1e741",
+    "url": "/static/js/2.1852aaa5.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/static/js/2.1852aaa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceac13d08ab9f818f027",
+    "url": "/static/js/main.cf9b1020.chunk.js"
+  },
+  {
+    "revision": "d2074b90921d905b4bbc",
+    "url": "/static/js/runtime-main.2bf7f533.js"
+  }
+]);
