9b481637a030c2447d32b45a7f70005a9f098b65
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-07-09 00:12:05 +0530

    add about section

--- z-project-one-portfolio/src/App.js
+++ z-project-one-portfolio/src/App.js
@@ -4,6 +4,8 @@ import Hero from './sections/Hero';
 import Main from './components/main';
 import 'react-typed/dist/animatedCursor.css'
 import Footer from './components/footer';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { } from "reactstrap"
 
 function App() {
   return (
