de2f4c1046d355d01290a016b8b6064ce8b85994
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-07-15 04:27:59 +0530

    added add / remove method

--- crash-todo/src/App.js
+++ crash-todo/src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
+
+  const [newItem, setNewItem] = useState("")
+  const [todo, setTodo] = useState([])
+
+  const addItem = item => {
+    if (item !== "") {
+      setNewItem({
+        id: Date.now(),
+        value: item,
+        isDone: false
+      })
+      setTodo([...todo, newItem])
+      setNewItem("")
+    }
+  }
+
+  const deleteItem = (id) => {
+    setTodo(todo.filter(item => item.id !== id))
+  }
+
+  const updateInput = (input) => {
+    setNewItem(input)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" width="100" height="100" className="logo" alt="logo" />
+      <h1 className="app-title">Todo App</h1>
+      <div className="container">
+        Add an item...
+        <br />
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Write a todo"
+          name="item"
+          required
+          value={newItem}
+          onChange={e => updateInput(e.target.value)}
+        />
+        <button
+          className="add-btn"
+          disabled={!newItem.length}
+          onClick={() => {
+            addItem(newItem)
+          }} > Add todo </button>
+        <div className="list">
+          <ul>
+            {todo.map(item => {
+
+              return (
+                <li key={item.index}>
+                  <input type="checkbox" name="" id="" checked={item.isDone} />
+                  {item.value}
+                  <button className="btn" >delete</button>
+                </li>
+              )
+
+            })}
+            <li >
+              <input type="checkbox" name="" id="" />
+                Record video
+                <button className="btn" >delete</button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
